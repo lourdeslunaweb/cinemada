c3cd170a93bb540a4f5b868d9d36e1c5dc9a5c87
Lourdes Luna <67447466+lourdeslunaweb@users.noreply.github.com>
2021-12-17 18:09:23 -0300

    footer done

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark container">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark container mb-3">
             <div className="container-fluid">
                 <div>
                     <h1 className="navbar-brand text-info fs-2 mt-3">CinemADA</h1>
@@ -8,7 +8,7 @@ const Navbar = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"/>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarColor02">
+                <div className="collapse navbar-collapse ms-5" id="navbarColor02">
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <button type="button" className="btn btn-outline-info rounded-pill m-1">Opcion 1</button>
