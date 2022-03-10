af559046900d11db083de265f738a241c0cc3e62
Lourdes Luna <67447466+lourdeslunaweb@users.noreply.github.com>
2022-03-09 23:20:26 -0300

    Movies are called from the Api

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -20,12 +20,13 @@ const Cards = () => {
                 {itemsArr?.map((item, index) => {
                     return (
                         <div key={index} className="card border-info m-3" style={{ width: '15rem'}}>
-                            <div className="card-header mt-1" style={{height:'5rem'}}><h5>{item.title}</h5></div>
+                            <div className="card-header mt-1" style={{height:'3rem'}}><h6>{item.title}</h6></div>
                             <div className="card-body">
-                                <img style={{ width: '10rem', height:'14rem' }} src={`http://image.tmdb.org/t/p/w500${item.poster_path}`} className="card-img-top mt-3" alt="afiche" />
+                                <img style={{ width: '8rem', height:'12rem' }} src={`http://image.tmdb.org/t/p/w500${item.poster_path}`} className="card-img-top mt-3" alt="afiche" />
                                 <button type="button" className="btn btn-outline-info mt-3">Puntaje: <span>{item.vote_average}</span></button>
                             </div>
-                            <button type="button" className="btn btn-info">Info</button>
+                            <button type="button" className="btn btn-info">Agregar</button>
+                            <button type="button" className="btn btn-outline-info">Eliminar</button>
                         </div>
                     )
                 })}
