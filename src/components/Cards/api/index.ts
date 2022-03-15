0ff42d196d07f08e6cd98848c610ec916516a538
Lourdes Luna <67447466+lourdeslunaweb@users.noreply.github.com>
2022-03-14 22:56:38 -0300

    addItem function is working

--- src/components/Cards/api/index.ts
+++ src/components/Cards/api/index.ts
@@ -1,8 +1,13 @@
-import { apiMovie } from "../../../utils";
+import { Item } from "../../../types";
+import { apiFirebase, apiMovie } from "../../../utils";
 
-const getContent = async ()  => {
+const getContent = async () => {
     const response = await apiMovie.get('/movie/top_rated');
     return (response.data.results)
 };
 
-export { getContent };
+const addItem = async (item: Item) => {
+    await apiFirebase.post("/items.json", item);
+};
+
+export { getContent, addItem };
--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,32 +1,37 @@
 import { useEffect, useState } from "react"
 import { Item } from "../../types";
 import { Loading } from "../Loading";
-import { getContent } from "./api";
+import { addItem, getContent } from "./api";
 
 const Cards = () => {
     const [itemsArr, setItemsArr] = useState<Item[]>()
     const [isLoading, setIsLoading] = useState(true)
     useEffect(() => {
         getContent().then((response) => {
-            console.log(response)
             setItemsArr(response)
             setIsLoading(false)
         });
     }, []);
+    const handleAddItem = (item: Item) => {
+        addItem(item)
+    }
+    const handleDeleteItem = (item: Item) => {
+        console.log(item)
+    }
     return (
         <>
             <div className="d-flex flex-wrap justify-content-center">
                 {isLoading ? <Loading /> : null}
                 {itemsArr?.map((item, index) => {
                     return (
-                        <div key={index} className="card border-info m-3" style={{ width: '15rem'}}>
-                            <div className="card-header mt-1" style={{height:'3rem'}}><h6>{item.title}</h6></div>
+                        <div key={index} className="card border-info m-3" style={{ width: '15rem' }}>
+                            <div className="card-header mt-1" style={{ height: '3rem' }}><h6>{item.title}</h6></div>
                             <div className="card-body">
-                                <img style={{ width: '8rem', height:'12rem' }} src={`http://image.tmdb.org/t/p/w500${item.poster_path}`} className="card-img-top mt-3" alt="afiche" />
+                                <img style={{ width: '10rem', height: '14rem' }} src={`http://image.tmdb.org/t/p/w500${item.poster_path}`} className="card-img-top mt-3" alt="afiche" />
                                 <button type="button" className="btn btn-outline-info mt-3">Puntaje: <span>{item.vote_average}</span></button>
                             </div>
-                            <button type="button" className="btn btn-info">Agregar</button>
-                            <button type="button" className="btn btn-outline-info">Eliminar</button>
+                                <button type="button" className="btn btn-outline-info" onClick={() => handleDeleteItem(item)}>Eliminar</button>
+                                <button type="button" className="btn btn-info" onClick={() => handleAddItem(item)}>Agregar</button>
                         </div>
                     )
                 })}
