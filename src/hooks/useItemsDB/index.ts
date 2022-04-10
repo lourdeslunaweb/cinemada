9a716d5c7d841b71ca2b99105ecd5db08a69df2f
lourdesluna <lourdes.luna@fluxit.com.ar>
2022-04-10 18:55:49 -0300

    home page in process

--- src/hooks/useItemsDB/index.ts
+++ src/hooks/useItemsDB/index.ts
@@ -3,31 +3,49 @@ import { mapToArray } from "../../helpers";
 import { Item } from "../../types";
 import { apiFirebase } from "../../utils";
 
-const useItemsDB = () =>{
-        // *** States ***
-        const [itemsArrDB, setItemsArrDB] = useState<Item[]>()
-        // *** Functions ***
-        const getItemsFromDB = async (): Promise<Item[]> => {
-            const response = await apiFirebase.get("/items.json");
-            return mapToArray(response.data)
-        };
+const useItemsDB = () => {
+    // *** States ***
+    const [itemsArrDB, setItemsArrDB] = useState<Item[]>()
+    // *** Functions ***
+    const getItemsFromDB = async (): Promise<Item[]> => {
+        const response = await apiFirebase.get("/items.json");
+        return mapToArray(response.data)
+    };
 
-        const upDateItemsDB = async () =>{
-            const response = await getItemsFromDB()
-            setItemsArrDB(response)
-        }
+    const upDateItemsDB = async () => {
+        const response = await getItemsFromDB()
+        console.log(response)
+        setItemsArrDB(response)
+    }
 
-        const addItemToDB = async (item: Item) => {
-            await apiFirebase.post("/items.json", item);
-            upDateItemsDB()
-        };
+    const addItemToDB = async (item: Item) => {
+        await apiFirebase.post("/items.json", item);
+        upDateItemsDB()
+    };
 
-        const deleteItemFromDB = async (id : number) => {
-            const itemToDelete = itemsArrDB?.find(item => item.id === id)
-            await apiFirebase.delete(`/items/${itemToDelete?.idDB}.json`)
-            upDateItemsDB()
-        }
-        return { getItemsFromDB , addItemToDB, upDateItemsDB, deleteItemFromDB, itemsArrDB};
+    const deleteItemFromDB = async (id: number) => {
+        const itemToDelete = itemsArrDB?.find(item => item.id === id)
+        await apiFirebase.delete(`/items/${itemToDelete?.idDB}.json`)
+        upDateItemsDB()
+    }
+
+    const checkItemDB = (id: number) => {
+        return true
+    }
+
+    const uncheckItemDB = (id: number) => {
+        return true
+    }
+
+    return {
+        getItemsFromDB,
+        addItemToDB,
+        upDateItemsDB,
+        deleteItemFromDB,
+        checkItemDB,
+        uncheckItemDB,
+        itemsArrDB,
+    };
 }
 
-export {useItemsDB}
+export { useItemsDB }
